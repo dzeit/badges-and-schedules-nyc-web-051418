033e06014a0685d0d5f4fcd1d0664cb4b7c16d25
David Zeit <dzeit216@gmail.com>
2018-05-13 03:06:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  array.map do |name|
+    badge_maker(name) #How the hell am I supposed to know this variable? Ask Laura.
+  end 
+end 
